d7b9c778d2d752295fd172684ffabaaafcf7e9b0
zeckrow <fadlidev99@gmail.com>
2026-03-07 13:36:52 +0700

    neovim main config"

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -28,7 +28,6 @@ keymap("i", "jk", "<Esc>", { desc = "Quit" })
 keymap("n", "<Tab>", ":bnext<CR>")
 keymap("n", "<S-Tab>", ":bprevious<CR>")
 --::
-
 --::floating terminal
 local term_buf = nil
 local term_win = nil
@@ -76,190 +75,57 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.formatoptions:remove({ "c", "r", "o" })
   end,
 })
---::
---::cursor highlight
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    if client.server_capabilities.documentHighlightProvider then
-      vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-        buffer = args.buf,
-        callback = vim.lsp.buf.document_highlight,
-      })
-      vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-        buffer = args.buf,
-        callback = vim.lsp.buf.clear_references,
-      })
-    end
-  end,
+--:: hightlight cursor
+vim.api.nvim_set_hl(0, "SearchMatch", { bg = "#3e4452", fg = "NONE", underline = true })
+local highlight_group = vim.api.nvim_create_augroup("BufferHighlight", { clear = true })
+vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+    group = highlight_group,
+    callback = function()
+        if vim.w.current_match_id then
+            pcall(vim.fn.matchdelete, vim.w.current_match_id)
+            vim.w.current_match_id = nil
+        end
+        local word = vim.fn.expand("<cword>")
+        if word ~= "" and word:match("^[a-zA-Z0-9_]+$") then
+            -- Gunakan pola regex agar hanya kata yang pas yang kena highlight
+            local pattern = [[\<]] .. word .. [[\>]]
+            -- Terapkan highlight ke seluruh buffer
+            vim.w.current_match_id = vim.fn.matchadd("SearchMatch", pattern, -1)
+        end
+    end,
 })
-vim.opt.updatetime = 300
---::
-
---::lsp setup
-vim.pack.add({
-  { src = "https://github.com/neovim/nvim-lspconfig" },
-  { src = "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim" },
-  { src = "https://github.com/mason-org/mason.nvim" },
-  { src = "https://github.com/nvim-tree/nvim-web-devicons" },
+vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+    group = highlight_group,
+    callback = function()
+        if vim.w.current_match_id then
+            pcall(vim.fn.matchdelete, vim.w.current_match_id)
+            vim.w.current_match_id = nil
+        end
+    end,
 })
-require("mason").setup()
-require("mason-tool-installer").setup({
-  ensure_installed = {
-    "lua-language-server",
-    "rust-analyzer",
-  },
-  integrations = {
-    ["mason-lspconfig"] = true,
-  },
-})
-
-vim.lsp.config("lua_ls", {
-  cmd = { "lua-language-server" },
-  filetypes = { "lua" },
-  root_markers = {
-    ".git",
-  },
-  settings = {
-    Lua = {
-      hint = { enable = true },
-      workspace = {
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-    },
-  },
-})
-vim.lsp.config("rust_analyzer", {
-  vim.lsp.config("rust_analyzer", {
-    root_dir = vim.fs.root(0, { "Cargo.toml", "rust-project.json", ".git" }),
-    settings = {
-      ["rust-analyzer"] = {
-        inlayHints = {
-          chainingHints = { enable = true },
-          closingBraceHints = { enable = true, minLines = 25 },
-          parameterHints = { enable = true },
-          typeHints = { enable = true },
-        },
-      },
-    },
-  }),
-})
-
-vim.keymap.set("n", "<leader>h", function()
-  local is_enabled = vim.lsp.inlay_hint.is_enabled()
-  vim.lsp.inlay_hint.enable(not is_enabled)
-  local status = not is_enabled and "Dinyalakan" or "Dimatikan"
-  print("Inlay Hints: " .. status)
-end, { desc = "Toggle LSP Inlay Hints" })
-
-vim.lsp.enable("lua_ls")
-vim.lsp.enable("rust_analyzer")
---::
-
---::completion and snippets
-vim.pack.add({
-  { src = "https://github.com/saghen/blink.cmp",            version = vim.version.range("^1") },
-  { src = "https://github.com/L3MON4D3/LuaSnip" },
-  { src = "https://github.com/rafamadriz/friendly-snippets" },
-})
-require("luasnip.loaders.from_vscode").lazy_load()
-require("blink.cmp").setup({
-  fuzzy = { implementation = "prefer_rust_with_warning" },
-  signature = { enabled = true },
-  appearance = {
-    use_nvim_cmp_as_default = true,
-    nerd_font_variant = "normal",
-  },
-  completion = {
-    documentation = {
-      auto_show = true,
-      auto_show_delay_ms = 200,
-    },
-  },
-
-  cmdline = {
-    keymap = {
-      preset = "inherit",
-      ["<CR>"] = { "accept_and_enter", "fallback" },
-    },
-  },
-
-  sources = { default = { "lsp", "path", "buffer", "snippets" } },
-})
---::
-
---::formater
-vim.pack.add({
-  { src = "https://github.com/stevearc/conform.nvim" },
-})
-require("conform").setup({
-  format_on_save = {
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
-  formatters_by_ft = {
-    lua = { "stylua" },
-    nix = { "nixfmt" },
-    json = { "jq" },
-    rust = { "rustfmt" },
-    python = { "black" },
-    htmldjango = { "djlint" },
-    html = { "djlint" },
-    javascript = { "prettier" },
-  },
-})
---::
-
---::autopairs
-vim.pack.add({
-  { src = "https://github.com/windwp/nvim-autopairs" },
-})
-require("nvim-autopairs").setup({})
---::
-
---::telecope
-vim.pack.add({
-  { src = "https://github.com/nvim-lua/plenary.nvim" },
-  { src = "https://github.com/nvim-telescope/telescope.nvim" },
-})
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
---::
-
---::colorscheme
-vim.pack.add({
-  { src = "https://github.com/rose-pine/neovim" },
-})
-vim.cmd.colorscheme("rose-pine")
---::
-
---::treesitter
-vim.pack.add({
-  { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
-  { src = "https://github.com/HiPhish/rainbow-delimiters.nvim" },
-})
-require("rainbow-delimiters.setup").setup({})
-vim.api.nvim_create_autocmd("FileType", {
-  callback = function(args)
-    -- 1. Daftar filetype yang harus diabaikan
-    local ignore_ft = { "TelescopePrompt", "TelescopeResults", "notify", "help", "qf", "fidget" }
-    for _, ft in ipairs(ignore_ft) do
-      if args.match == ft then
-        return
-      end
-    end
-    local lang = vim.treesitter.language.get_lang(args.match) or args.match
-    if lang == "" or lang == nil then
-      return
-    end
-    local ok, _ = pcall(vim.treesitter.language.inspect, lang)
-    if not ok then
-      local ts_ok, ts = pcall(require, "nvim-treesitter")
-      if ts_ok then
-        pcall(ts.install, lang, { sync = false })
-      end
-    end
-    pcall(vim.treesitter.start)
-  end,
-})
---::
+--: auto pairs
+local keymap = vim.keymap.set
+local opts = { noremap = true, silent = true }
+local char_pairs = {
+    ['('] = ')',
+    ['['] = ']',
+    ['{'] = '}',
+    ['"'] = '"',
+    ["'"] = "'",
+    ['`'] = '`',
+}
+for open, close in pairs(char_pairs) do
+    keymap('i', open, function()
+        local line = vim.api.nvim_get_current_line()
+        local col = vim.api.nvim_win_get_cursor(0)[2]
+        local char_before = line:sub(col, col)
+        local char_after = line:sub(col + 1, col + 1)
+        if open == close and char_after == close then
+            if char_before == open then
+                return open .. close .. '<Left>'
+            end
+            return '<Right>'
+        end
+        return open .. close .. '<Left>'
+    end, { expr = true, noremap = true })
+end
