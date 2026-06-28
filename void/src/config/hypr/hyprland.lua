237e45781147720e04a8daffbd0066d5cabd2d12
rowsred <fadlidev99@gmail.com>
2026-06-28 13:40:24 +0000

    update config

--- void/src/config/hypr/hyprland.lua
+++ void/src/config/hypr/hyprland.lua
@@ -46,11 +46,11 @@ local menu = "hyprlauncher"
 -- Autostart necessary processes (like notifications daemons, status bars, etc.)
 -- Or execute your favorite apps at launch like this:
 --
--- hl.on("hyprland.start", function ()
---   hl.exec_cmd(terminal)
---   hl.exec_cmd("nm-applet")
---   hl.exec_cmd("waybar & hyprpaper & firefox")
--- end)
+hl.on("hyprland.start", function()
+	hl.exec_cmd("dbus-launch waybar &")
+	--   hl.exec_cmd("nm-applet")
+	--   hl.exec_cmd("waybar & hyprpaper & firefox")
+end)
 
 -------------------------------
 ---- ENVIRONMENT VARIABLES ----
