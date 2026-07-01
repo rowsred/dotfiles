ffc4ee824a53e0c83afec4ba9d76539f17f6e2a0
rowsred <fadlidev99@gmail.com>
2026-07-01 03:43:39 +0000

    add kwm

--- flake.nix
+++ flake.nix
@@ -3,22 +3,30 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+    river-kwm.url = "github:rowsred/river_kwm_modules_nixos";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs =
+    {
+      self,
+      nixpkgs,
+      river-kwm,
+    }:
+    {
 
-    nixosConfigurations = {
-      nixos = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./src/configuration.nix
-          ./src/hyprland.nix
-          ./src/editor.nix
-        ];
+      nixosConfigurations = {
+        nixos = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit river-kwm; };
+          system = "x86_64-linux";
+          modules = [
+            ./src/configuration.nix
+            ./src/kwm.nix
+            ./src/editor.nix
+          ];
+
+        };
 
       };
 
     };
-
-  };
 }
